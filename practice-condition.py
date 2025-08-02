6df66f442e097e18257f5867af346b3eca3346cd
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-08-03 00:22:27 +0600

    added if con..

---
+++ practice-condition.py
@@ -0,0 +1,21 @@
+#
+# conditional statement if
+marks=80
+if marks>=88 and marks<=100:
+    print('A+')
+elif marks>=70 and marks<=80:
+    print('A-')
+elif marks>=60 and marks<=70:
+    print('B')
+
+elif marks>=50 and marks<=60:
+    print('B')
+
+elif marks>=40 and marks<=50:
+    print('c')
+
+elif marks>=33 and marks<=40:
+    print('D')
+
+else:
+    print('F')
