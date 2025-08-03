82688020589e0087496bcd23372ca945d956f023
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-08-04 01:45:08 +0600

    added    oop

---
+++ oop.py
@@ -0,0 +1,31 @@
+# class
+
+
+class Myclss:
+    x=10
+    y=30
+    z=40
+    sum=x+y+z
+
+
+obj1=Myclss()                #aitake object bola  hoy
+print(obj1.x)
+
+
+obj2=Myclss()                #aitake object bola  hoy
+print(obj1.y)
+
+
+
+obj2=Myclss()                #aitake object bola  hoy
+print(obj1.z)
+
+
+
+
+
+obj1=Myclss()                #aitake object bola  hoy
+print(obj1.x)
+print(obj1.y)
+print(obj1.z)
+print(obj1.sum)
