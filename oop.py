6102d739ebff0ce6766e33c0fa4f7c9e4a33b7f2
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-08-04 19:41:38 +0600

    added   python oop

--- oop.py
+++ oop.py
@@ -1,31 +1,58 @@
-# class
+# # class
+#
+#
+# class Myclss:          #  clss   er    nam ta  capital letter  a   ditye  hoy
+#     x=10
+#     y=30
+#     z=40
+#     sum=x+y+z
+#
+#
+# obj1=Myclss()                #aitake object bola  hoy
+# print(obj1.x)
+#
+#
+# obj2=Myclss()                #aitake object bola  hoy
+# print(obj1.y)
+#
+#
+#
+# obj2=Myclss()                #aitake object bola  hoy
+# print(obj1.z)
+#
+#
+# obj1=Myclss()                #aitake object bola  hoy
+# print(obj1.x)
+# print(obj1.y)
+# print(obj1.z)
+# print(obj1.sum)
 
 
-class Myclss:
+
+class Myclss:          #  clss   er    nam ta  capital letter  a   ditye  hoy
     x=10
-    y=30
-    z=40
-    sum=x+y+z
+    y=20
+    z=30
+
+    def addTwo(self,a,b):
+        sum=self.x+self.y+self.z+a+b
+        print(sum)
+
+    def addNew(self):
+        self.addTwo(5,6)
 
 
-obj1=Myclss()                #aitake object bola  hoy
-print(obj1.x)
-
-
-obj2=Myclss()                #aitake object bola  hoy
-print(obj1.y)
-
-
-
-obj2=Myclss()                #aitake object bola  hoy
-print(obj1.z)
-
+obj =Myclss()
+obj.addTwo(30,50)
+obj.addNew()
 
 
 
 
-obj1=Myclss()                #aitake object bola  hoy
-print(obj1.x)
-print(obj1.y)
-print(obj1.z)
-print(obj1.sum)
+
+
+
+
+
+
+
